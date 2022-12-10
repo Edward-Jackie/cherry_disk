5bcdcfcb3a3a08ad7b986bc8dec0ebede62b72a3
edj <419130032@qq.com>
2022-12-11 06:06:36 +0800

    Send Identity Code By Mail Test

---
+++ test/mail_test.go
@@ -0,0 +1,22 @@
+package test
+
+import (
+	"cherry-disk/core/define"
+	"crypto/tls"
+	"github.com/jordan-wright/email"
+	"net/smtp"
+	"testing"
+)
+
+func TestSendMail(t *testing.T) {
+	e := email.NewEmail()
+	e.From = "Get <iamyunjielai@163.com>"
+	e.To = []string{"419130032@qq.com"}
+	e.Subject = "验证码发送测试"
+	e.HTML = []byte("你的验证码为：<h1>123456</h1>")
+	err := e.SendWithTLS("smtp.163.com:465", smtp.PlainAuth("", "iamyunjielai@163.com", define.MailPWD, "smtp.163.com"),
+		&tls.Config{InsecureSkipVerify: true, ServerName: "smtp.163.com"})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
