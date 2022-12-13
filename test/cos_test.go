54672cd33f008f7ca6ec61ba39a10cc24f72df16
edj <419130032@qq.com>
2022-12-13 22:59:31 +0800

    upload img

--- test/cos_test.go
+++ test/cos_test.go
@@ -11,11 +11,8 @@ import (
 )
 
 func TestFileUploadByFilePath(t *testing.T) {
-	u, _ := url.Parse("https://cherryneko-1312558494.cos.ap-nanjing.myqcloud.com ")
-	b := &cos.BaseURL{
-		BucketURL: u,
-	}
-
+	u, _ := url.Parse("https://cherryneko-1312558494.cos.ap-nanjing.myqcloud.com")
+	b := &cos.BaseURL{BucketURL: u}
 	client := cos.NewClient(b, &http.Client{
 		Transport: &cos.AuthorizationTransport{
 			SecretID:  define.TencentSecretID,
@@ -23,9 +20,11 @@ func TestFileUploadByFilePath(t *testing.T) {
 		},
 	})
 
-	key := "cherrydisk/example.jpg"
+	key := "cherry-disk/neko.jpg"
 
-	_, _, err := client.Object.Upload(context.Background(), key, "./img/cherryneko.jpg", nil)
+	_, _, err := client.Object.Upload(
+		context.Background(), key, "./img/neko.jpg", nil,
+	)
 	if err != nil {
 		panic(err)
 	}
