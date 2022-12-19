b68c590259a4a3b4c4e6fd6a46fc4311dd665471
edj <419130032@qq.com>
2022-12-20 00:32:05 +0800

    ShardFile Test

--- test/cos_test.go
+++ test/cos_test.go
@@ -3,8 +3,11 @@ package test
 import (
 	"cherry-disk/core/common"
 	"context"
+	"math"
 	"net/http"
 	"net/url"
+	"os"
+	"strconv"
 	"testing"
 
 	"github.com/tencentyun/cos-go-sdk-v5"
@@ -30,4 +33,47 @@ func TestFileUploadByFilePath(t *testing.T) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+var chunkSize = 10 * 1024 * 1024 // 10MB
+
+// 文件分片
+func TestGeneralChunkFile(t *testing.T) {
+	file, err := os.OpenFile("./mv/test.mp4", os.O_RDONLY, 0666)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fileInfo, err := os.Stat("./mv/test.mp4")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	//分片的个数
+	chunkNum := math.Ceil(float64(fileInfo.Size()) / float64(chunkSize))
+	b := make([]byte, chunkSize)
+	for i := 0; i < int(chunkNum); i++ {
+		file.Seek(int64(i*chunkSize), 0)
+		if int64(chunkSize) > fileInfo.Size()-int64(i*chunkSize) {
+			b = make([]byte, fileInfo.Size()-int64(i*chunkSize))
+		}
+
+		file.Read(b)
+		f, err := os.OpenFile("./chunks/"+strconv.Itoa(i)+".chunk", os.O_CREATE|os.O_WRONLY, os.ModePerm)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		f.Write(b)
+		f.Close()
+	}
+	file.Close()
+}
+
+// 文件合并 - 校验一致性
+func TestChunkFile(t *testing.T) {
+	//file, err := os.OpenFile("test.mp4", os.O_CREATE|os.O_WRONLY|os.O_APPEND, os.ModePerm)
+	//if err != nil {
+	//	t.Fatal(err)
+	//}
 }
